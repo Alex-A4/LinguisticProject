2b9b3c99afa7866727b5b8e6af70861d46cc92fd
Alex Adrianov <alex.a4.25@yandex.ru>
2018-09-21 18:00:36 +0300

    Added implementation of communication between presenter and LogInView

--- src/main/java/com/alexa4/linguistic_project/presenter/Presenter.java
+++ src/main/java/com/alexa4/linguistic_project/presenter/Presenter.java
@@ -1,18 +1,22 @@
 package com.alexa4.linguistic_project.presenter;
 
 import com.alexa4.linguistic_project.models.Model;
-import com.alexa4.linguistic_project.view.MainView;
 import com.alexa4.linguistic_project.view.ViewInterface;
+import com.alexa4.linguistic_project.view.LessonsView;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * This class needs to link model and view
  */
 public class Presenter {
 
+    private final Stage stage;
     private final Model model;
     private ViewInterface view = null;
 
-    public Presenter(Model model) {
+    public Presenter(Model model, Stage stage) {
+        this.stage = stage;
         this.model = model;
     }
 
@@ -36,5 +40,32 @@ public class Presenter {
      */
     public void getText(){
         view.setText(model.getText());
+    }
+
+    /**
+     * Trying to login by check does this account contains in DataBase
+     * @param userName the name of user account
+     * @param userPassword the password of user account
+     * @return is this account contains in DB
+     */
+    public void tryToLogIn(String userName, String userPassword){
+        model.tryToLogIn(userName, userPassword, new Model.LogInCallback() {
+            @Override
+            public void sendLogInResponse(boolean response) {
+                if (response)
+                    view.logIn();
+                else view.logInError();
+            }
+        });
+    }
+
+
+    /**
+     * Starting lessonsView which needs to work
+     */
+    public void startLessonsView(){
+        view.detachPresenter();
+        view = new LessonsView(this);
+        stage.setScene(new Scene(((LessonsView) view).getLayout()));
     }
 }
