0e5bfa51031632b752921bcd538674938e49946e
Alex Adrianov <alex.a4.25@yandex.ru>
2018-10-14 16:07:22 +0300

    Change comment

--- src/main/java/com/alexa4/linguistic_project/data_stores/User.java
+++ src/main/java/com/alexa4/linguistic_project/data_stores/User.java
@@ -4,7 +4,7 @@ public class User {
     private final String userName;
     private final int userMode;
 
-    //The teacher can check the students work and evaluate it
+    //The teacher can check the student work and evaluate it
     public static final int TEACHER_MODE = 1;
 
     //The student can solve task and send the result
