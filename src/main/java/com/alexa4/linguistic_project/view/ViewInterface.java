5a002142ba64bad20795997e8309706ea29037db
Alex Adrianov <alex.a4.25@yandex.ru>
2018-10-14 16:23:27 +0300

    Deleted unless methods

--- src/main/java/com/alexa4/linguistic_project/view/ViewInterface.java
+++ src/main/java/com/alexa4/linguistic_project/view/ViewInterface.java
@@ -7,35 +7,10 @@ import javafx.scene.Parent;
  */
 public interface ViewInterface {
     /**
-     * Setting the text to specified text area
-     * @param text which need to set somewhere
-     */
-    default void setText(String text){}
-
-    /**
-     * Call view method if logging response is true
-     */
-    default void logIn(){}
-
-    /**
-     * Call view method if logging response is false
-     */
-    default void logInError(){}
-
-    /**
      * Default method to detach presenters from view to lost link
      */
     void detachPresenter();
 
-    /**
-     * Call view method if sign in response is true
-     */
-    default void signIn(){}
-
-    /**
-     * Call view method if sign in response is false
-     */
-    default void signInError(){}
 
     /**
      * Default method to get basic layout
