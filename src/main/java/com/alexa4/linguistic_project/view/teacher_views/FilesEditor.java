13654723dd3a8baab901c59660312d944173a313
Alex Adrianov <alex.a4.25@yandex.ru>
2018-10-22 20:10:40 +0300

    Now is a subclass of ViewTextInterface, implements abstract methods

--- src/main/java/com/alexa4/linguistic_project/view/teacher_views/FilesEditor.java
+++ src/main/java/com/alexa4/linguistic_project/view/teacher_views/FilesEditor.java
@@ -1,40 +1,24 @@
 package com.alexa4.linguistic_project.view.teacher_views;
 
-import com.alexa4.linguistic_project.data_stores.MeansOfExpressiveness;
 import com.alexa4.linguistic_project.presenters.teacher.TeacherPresenter;
-import com.alexa4.linguistic_project.view.ViewInterface;
 import com.alexa4.linguistic_project.view.ViewTextInterface;
-import com.sun.istack.internal.NotNull;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Cursor;
 import javafx.scene.control.*;
-import javafx.scene.input.ContextMenuEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import org.fxmisc.richtext.StyleClassedTextArea;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class FilesEditor implements ViewTextInterface {
+public class FilesEditor extends ViewTextInterface {
     private TeacherPresenter mPresenter;
-    private StyleClassedTextArea area;
-    private VBox layout;
-    private VBox choiceField;
     private TextField fileNameTF;
 
-    private static final int CHOICE_FIELD_WIDTH = 350;
-    private static final int WINDOW_LEFT_PADDING = 30;
-    private static final int WINDOW_RIGHT_PADDING = 30;
-
     public FilesEditor(TeacherPresenter presenter) {
+        super();
         this.mPresenter = presenter;
     }
 
@@ -44,203 +28,27 @@ public class FilesEditor implements ViewTextInterface {
     }
 
     @Override
-    public VBox getLayout() {
-        layout = new VBox(10);
-        initWindow();
-
-        return layout;
+    protected void addUserChoice(String means, String selectedText) {
+        mPresenter.addUserChoice(means, selectedText);
     }
 
-
-    /**
-     * Initializing text area
-     */
-    private StyleClassedTextArea initTextField(){
-        area = new StyleClassedTextArea();
-        area.setWrapText(true);
-        area.setEditable(true);
-        area.setPadding(new Insets(10, 10, 10, 10));
-        area.setPrefSize(600, 400);
-        area.setBorder(new Border(new BorderStroke(
-                Paint.valueOf("#000000"), BorderStrokeStyle.SOLID,  CornerRadii.EMPTY,
-                BorderWidths.DEFAULT
-        )));
-
-        //Setting pop-up menu, which will offer user select one of Means of expressiveness
-        ContextMenu menu = initContextMenu();
-
-        area.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
-            @Override
-            public void handle(ContextMenuEvent event) {
-                menu.show(area, event.getScreenX(),
-                        event.getScreenY());
-            }
-        });
-
-        return area;
+    @Override
+    protected Paint getTextColors() {
+        return mPresenter.getTextColors();
     }
 
-    /**
-     * Initializing pop-up menu with means of expressiveness
-     * @return pop-up menu
-     */
-    private ContextMenu initContextMenu(){
-        ContextMenu menu = new ContextMenu();
-
-        for (MeansOfExpressiveness means : MeansOfExpressiveness.values()) {
-            //Initializing menuItem with title of Means. First character is in up case
-            MenuItem item = new MenuItem(means.getText().substring(0, 1)
-                    .toUpperCase().concat(means.getText().substring(1)));
-            //If user choose some selected text, then add he's choice to choiceField and to collection
-            item.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-
-                    //If selected text is not empty, then display user choice
-                    if (!area.getSelectedText().trim().equals("")) {
-                        addUserChoice(item.getText(), area.getSelectedText());
-                    }
-                }
-            });
-            menu.getItems().add(item);
-        }
-
-        return menu;
+    @Override
+    protected void deleteUserChoice(String means, String selectedText) {
+        mPresenter.deleteUserChoice(means, selectedText);
     }
 
-    /**
-     * Display user choice in choiceField like a record
-     *
-     * Make new record box which contains:
-     * ------------------------------
-     * textMeans        |  Button X |
-     * userSelectedText |           |
-     * ------------------------------
-     *
-     */
-    private void addUserChoice(String means, String text) {
-        HBox newRecordBox = new HBox(10);
-        newRecordBox.setAlignment(Pos.CENTER_LEFT);
-
-        Text textMeans = new Text();
-        Text userSelectedText = new Text();
-
-        //Button X will delete current box from list
-        Button deleteRecordButton = new Button("X");
-        deleteRecordButton.setStyle("-fx-background-color: #00000000;");
-        deleteRecordButton.setCursor(Cursor.CLOSED_HAND);
-        deleteRecordButton.setTextFill(Paint.valueOf("#ff0000"));
-        deleteRecordButton.setFont(new Font(20));
-
-        //If user pressed X button, then delete this box and delete user's choice
-        //which connect with that record
-        deleteRecordButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                mPresenter.deleteUserChoice(means.toLowerCase(),
-                        userSelectedText.getText());
-                choiceField.getChildren().remove(newRecordBox);
-            }
-        });
-
-
-        //Box with means name and with text which user selected
-        VBox textBox = new VBox();
-
-        //textMeans will display which means of expressiveness user select
-        textMeans.setText("\n" + means.toUpperCase() +
-                " is");
-        textMeans.setWrappingWidth(CHOICE_FIELD_WIDTH - 70);
-        textMeans.setFill(mPresenter.getTextColors());
-
-        //userSelectedText will display which text user select
-        userSelectedText.setWrappingWidth(CHOICE_FIELD_WIDTH - 70);
-        userSelectedText.setText(text.trim());
-
-        //If user clicked on textBox, then the text will be selected in area
-        textBox.getChildren().addAll(textMeans, userSelectedText);
-        textBox.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                int start = area.getText().indexOf(userSelectedText.getText());
-                int end = userSelectedText.getText().length();
-                area.selectRange(start, start + end);
-            }
-        });
-        newRecordBox.getChildren().addAll(textBox, deleteRecordButton);
-
-        //Add current text to collection
-        mPresenter.addUserChoice(means.toLowerCase(), text);
-
-        //Add current box to view
-        choiceField.getChildren().addAll(newRecordBox);
-    }
-
-
-    /**
-     * Initializing window by the content, now:
-     * TextArea, button to show text
-     */
-    private void initWindow() {
-        HBox textBox = new HBox(15);
-        textBox.setPadding(new Insets(0, WINDOW_RIGHT_PADDING, 30, WINDOW_LEFT_PADDING));
-
-        area = initTextField();
-
-        HBox menuBar = initMenuBar();
-
-        choiceField = initChoiceVBox();
-        ScrollPane choicePane = new ScrollPane();
-        choicePane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        choicePane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        choicePane.setContent(choiceField);
-        choicePane.setBorder(new Border(new BorderStroke(
-                Paint.valueOf("#000000"), BorderStrokeStyle.SOLID,  CornerRadii.EMPTY,
-                BorderWidths.DEFAULT
-        )));
-
-        HBox textBoxLabels = initTextBoxLabels();
-
-        textBox.getChildren().addAll(area, choicePane);
-        
-        HBox fileActions = initFileActions();
-        
-        layout.getChildren().addAll(menuBar, textBoxLabels, textBox, fileActions);
-    }
-
-
-    /**
-     * Initializing box which contains labels for text box
-     * @return the labelsBox
-     */
-    private HBox initTextBoxLabels() {
-        HBox mLabelBox = new HBox(5);
-        mLabelBox.setPadding(new Insets(15, WINDOW_RIGHT_PADDING, 5, WINDOW_LEFT_PADDING));
-
-        Label textLabel = new Label("Text");
-        textLabel.setFont(new Font(18));
-        HBox textBox = new HBox();
-        textBox.getChildren().add(textLabel);
-        textBox.setAlignment(Pos.CENTER_LEFT);
-
-        Label choiceLabel = new Label("Selected means of expression");
-        choiceLabel.setFont(new Font(18));
-        HBox choiceBox = new HBox();
-        choiceBox.getChildren().add(choiceLabel);
-        choiceBox.setAlignment(Pos.CENTER_RIGHT);
-
-        mLabelBox.setHgrow(textBox, Priority.ALWAYS);
-        mLabelBox.setHgrow(choiceBox, Priority.ALWAYS);
-        mLabelBox.getChildren().addAll(textBox, choiceBox);
-
-        return mLabelBox;
-    }
 
     /**
      * Box contains TF with file name and save button
      * @return the box
      */
-    private HBox initFileActions() {
+    @Override
+    protected HBox initUserActions() {
         HBox fileActions = new HBox(20);
         fileActions.setPadding(new Insets(0, WINDOW_RIGHT_PADDING, 30, WINDOW_LEFT_PADDING));
 
@@ -280,12 +88,23 @@ public class FilesEditor implements ViewTextInterface {
         return fileActions;
     }
 
+    @Override
+    protected void getText(String fileName) {
+        mPresenter.getText(fileName);
+    }
+
+    @Override
+    protected void clearUserChoice() {
+        mPresenter.clearUserChoice();
+    }
+
 
     /**
      * Initializing menu bar
      * @return the menu bar
      */
-    private HBox initMenuBar() {
+    @Override
+    protected HBox initMenuBar() {
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().addAll(createTasksMenu(), createWindowsMenu());
 
@@ -294,6 +113,16 @@ public class FilesEditor implements ViewTextInterface {
         barBox.getChildren().addAll(menuBar);
 
         return barBox;
+    }
+
+    @Override
+    protected String getUserName() {
+        return mPresenter.getUserName();
+    }
+
+    @Override
+    protected int getUserMode() {
+        return mPresenter.getUserMode();
     }
 
     private Menu createWindowsMenu() {
@@ -315,7 +144,7 @@ public class FilesEditor implements ViewTextInterface {
         //Menu item response for creating new file
         MenuItem newFile = new MenuItem("Add new file");
         newFile.setOnAction(event -> {
-            freeUserChoice();
+            freeUserChoices();
             area.clear();
             fileNameTF.clear();
         });
@@ -345,81 +174,10 @@ public class FilesEditor implements ViewTextInterface {
      * Firstly userChoice is clearing and then filling by existence means
      */
     private void fillUserChoice() {
-        freeUserChoice();
+        freeUserChoices();
 
         HashMap<String, ArrayList<String>> foundMeans = mPresenter.getFoundMeans();
         foundMeans.forEach((means, collection) -> collection
-                .forEach(text -> addUserChoice(means, text)));
-    }
-
-
-    /**
-     * Clearing choiceField
-     */
-    private void freeUserChoice() {
-        mPresenter.clearUserChoice();
-
-        //Clear choiceField
-        int count = choiceField.getChildren().size();
-        for (int i = 0; i < count; i++)
-            choiceField.getChildren().remove(0);
-    }
-
-
-    /**
-     * Initializing text file where will display user's choice
-     * @return
-     */
-    private VBox initChoiceVBox() {
-        choiceField = new VBox();
-
-        choiceField.setPrefWidth(CHOICE_FIELD_WIDTH);
-        choiceField.setMaxWidth(CHOICE_FIELD_WIDTH);
-
-        choiceField.setPadding(new Insets(10, 10, 10, 10));
-
-        return choiceField;
-    }
-
-
-    /**
-     * Call Dialog Alert if something happens
-     * @param title the title of Alert
-     * @param header the header of Alert
-     * @param content the content text of Alert
-     */
-    private void callAlert(@NotNull String title, String header, @NotNull String content){
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle(title);
-        if (header != null)
-            alert.setHeaderText(header);
-        alert.setContentText(content);
-        alert.showAndWait();
-    }
-
-    /**
-     * Call Dialog Alert if action completed successfully
-     * @param title the title of Alert
-     * @param header the header of Alert
-     * @param content the context of Alert
-     */
-    private void callSuccess(@NotNull String title, String header, @NotNull String content) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle(title);
-        if (header != null)
-            alert.setHeaderText(header);
-        alert.setContentText(content);
-        alert.showAndWait();
-    }
-
-    /**
-     * Setting text to StyleClassedTextArea
-     * @param text
-     */
-    @Override
-    public void setText(String text) {
-        area.clear();
-        area.appendText(text);
-        area.scrollToPixel(0, 0);
+                .forEach(text -> addUserChoiceToBox(means, text)));
     }
 }
