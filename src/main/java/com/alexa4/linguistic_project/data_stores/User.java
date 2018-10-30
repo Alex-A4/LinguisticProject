0d4b45aff40d3d7f4bcb28bb88cb18a10e17728b
Alex Adrianov <alex.a4.25@yandex.ru>
2018-10-30 10:10:34 +0300

    Added field to store last and first user names. Added more comments

--- src/main/java/com/alexa4/linguistic_project/data_stores/User.java
+++ src/main/java/com/alexa4/linguistic_project/data_stores/User.java
@@ -1,8 +1,12 @@
 package com.alexa4.linguistic_project.data_stores;
 
 public class User {
-    private final String userName;
+    //Variable of user login
+    private final String mUserLogin;
+    //Variable which contains mode of user
     private final int userMode;
+    //Variable which contains LastName + FirstName
+    private final String mLastFirstNames;
 
     //The teacher can check the student work and evaluate it
     public static final int TEACHER_MODE = 1;
@@ -12,19 +16,24 @@ public class User {
 
     /**
      * Init user with name and mode
-     * @param userName
-     * @param userMode
+     * @param userLogin the login name of user
+     * @param userMode the mode of user
      */
-    public User (String userName, int userMode) {
-        this.userName = userName;
+    public User (String userLogin, int userMode, String lastFirstNames) {
+        this.mUserLogin = userLogin;
+        this.mLastFirstNames = lastFirstNames;
 
         if (userMode == TEACHER_MODE || userMode == STUDENT_MODE)
             this.userMode = userMode;
         else this.userMode = STUDENT_MODE;
     }
 
+    public String getLastFirstNames() {
+        return mLastFirstNames;
+    }
+
     public String getUserName() {
-        return userName;
+        return mUserLogin;
     }
 
     public int getUserMode() {
