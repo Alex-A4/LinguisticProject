035693fc552a932268c8a6bc31100b05b38aa87d
Alex <alex.A4.25@yandex.ru>
2018-11-15 22:44:45 +0300

    Fixed nullPointerException when answersMap is getting

--- src/main/java/com/alexa4/linguistic_project/models/TextModel.java
+++ src/main/java/com/alexa4/linguistic_project/models/TextModel.java
@@ -20,6 +20,7 @@ public class TextModel {
 
     //List of files with tasks
     private ArrayList<String> mTasksFilesNameList = null;
+
     public List<String> getTasksListOfFiles() {
         return mTasksFilesNameList;
     }
@@ -34,6 +35,7 @@ public class TextModel {
     //Collection with the means found in the current text:
     //Key - is a means name, value - list of sentences with this means
     private HashMap<String, ArrayList<String>> mFoundedMeans;
+
     public HashMap<String, ArrayList<String>> getFoundedMeans() {
         return this.mFoundedMeans;
     }
@@ -66,6 +68,7 @@ public class TextModel {
 
     /**
      * Reading files with students answer
+     *
      * @return the list with all answers
      */
     private ArrayList<String> readAnswerFiles() {
@@ -86,14 +89,13 @@ public class TextModel {
     }
 
 
-
-
     /**
      * Reading text from the file and initializing foundedMeans by the means which contains into
      * the current text
+     *
      * @return non marked text, which will be shown to user
      */
-    public String getText(String taskName){
+    public String getText(String taskName) {
         mFoundedMeans = new HashMap<String, ArrayList<String>>();
         mCurrentTaskName = taskName;
 
@@ -105,7 +107,7 @@ public class TextModel {
 
             scanner.useDelimiter("\n");
 
-            while(scanner.hasNext()) {
+            while (scanner.hasNext()) {
                 builder.append(scanner.next());
                 builder.append("\n");
             }
@@ -128,18 +130,19 @@ public class TextModel {
     /**
      * Verify the correctness of the user answers
      * If the mark from the user marking is from 80% to 120% of original marking, then the mark is right
+     *
      * @param originalMarking the original marking of the task
-     * @param userMarking the marking which user select
+     * @param userMarking     the marking which user select
      * @return the collections of verified answers looks like: textOfChoice - boolean correctness
      */
     public HashMap<String, Boolean> verifyAndGetCorrectnessOfAnswers(HashMap<String, ArrayList<String>> originalMarking,
-                                           HashMap<String, ArrayList<String>> userMarking) {
+                                                                     HashMap<String, ArrayList<String>> userMarking) {
         //Collection which contains pairs: text of means of expression - correctness of user choice
         HashMap<String, Boolean> verifiedAnswers = new HashMap<>();
 
         //Comparing each text from userMarking with each text from originalMarking
         userMarking.forEach((userMeans, userTextList) -> {
-            userTextList.forEach((text) ->{
+            userTextList.forEach((text) -> {
                 verifiedAnswers.put(text,
                         isUserTextCorrectness(text, originalMarking.get(userMeans)));
             });
@@ -152,24 +155,25 @@ public class TextModel {
     /**
      * Comparing text of user choice and each text of original marking
      * If the mark from the user marking is from 80% to 120% of original marking, then the mark is right
-     * @param userText the text of user which need verify
+     *
+     * @param userText      the text of user which need verify
      * @param originalMeans the list of text from originalMarking
      * @return is the userText correct
      */
     private boolean isUserTextCorrectness(String userText, ArrayList<String> originalMeans) {
         if (originalMeans == null)
             return false;
-        for (String originalText: originalMeans) {
+        for (String originalText : originalMeans) {
             //If original text is fully equals to userText
             if (originalText.equals(userText))
                 return true;
 
             //If userText is in originalText and userText length more then 80% of originalText length
-            if (originalText.contains(userText) && ((double) userText.length())/originalText.length() * 100 > 80.0)
+            if (originalText.contains(userText) && ((double) userText.length()) / originalText.length() * 100 > 80.0)
                 return true;
 
             //If userText contains originalText and userText length not more then 120% of originalText length
-            if (userText.contains(originalText) && ((double) userText.length())/originalText.length() * 100 < 120.0)
+            if (userText.contains(originalText) && ((double) userText.length()) / originalText.length() * 100 < 120.0)
                 return true;
         }
 
@@ -177,11 +181,10 @@ public class TextModel {
     }
 
 
-
-
     /**
      * Saving the file changes
-     * @param text the text which need to save
+     *
+     * @param text     the text which need to save
      * @param fileName the name of file
      * @return writing status
      */
@@ -202,8 +205,9 @@ public class TextModel {
 
     /**
      * Writing bytes to file
+     *
      * @param filePath the full path of file which need to save
-     * @param text the text which need to write into file
+     * @param text     the text which need to write into file
      */
     private void writeTextToFile(String filePath, String fileName, String text) throws Exception {
         new File(filePath).mkdirs();
@@ -215,7 +219,8 @@ public class TextModel {
 
     /**
      * Converting choice without marks to marked text
-     * @param means the means choice
+     *
+     * @param means  the means choice
      * @param choice the choice text
      */
     private void tryToAddMeansToText(String means, String choice) {
@@ -230,11 +235,10 @@ public class TextModel {
     }
 
 
-
-
     /**
      * Saving user's answers to file which looks like taskName_userName
      * mMarkedText is rewriting, so could not be used later with previous value
+     *
      * @return the result of saving
      */
     public boolean saveUserAnswer() {
@@ -261,6 +265,7 @@ public class TextModel {
      * Decoding file name to array of
      * [0] name of task
      * [1] login of student
+     *
      * @param fileName the name of decoding file
      * @return the decoded name of file
      */
@@ -268,7 +273,7 @@ public class TextModel {
         String[] objects = new String[2];
         int underLinePos = fileName.indexOf("_");
         objects[0] = fileName.substring(0, underLinePos);
-        objects[1] = fileName.substring(underLinePos+1);
+        objects[1] = fileName.substring(underLinePos + 1);
 
         return objects;
     }
@@ -284,8 +289,9 @@ public class TextModel {
 
     /**
      * Add user's choice to collection
+     *
      * @param means the means which user choose
-     * @param text the text which user select
+     * @param text  the text which user select
      */
     public void addUserChoice(String means, String text) {
         if (mUserChoiceList.containsKey(means))
@@ -299,8 +305,9 @@ public class TextModel {
 
     /**
      * Delete user choice from collection
+     *
      * @param means the removable means
-     * @param text the removable text
+     * @param text  the removable text
      */
     public void deleteUserChoice(String means, String text) {
         ArrayList<String> temp = mUserChoiceList.get(means);
@@ -311,10 +318,9 @@ public class TextModel {
     }
 
 
-
-
     /**
      * Get random color which will set somewhere
+     *
      * @return new color
      */
     public Paint getRandomColor() {
@@ -327,27 +333,36 @@ public class TextModel {
 
     /**
      * Generate color char from 0 to f in dex
+     *
      * @return next color char
      */
     private String getNextColorChar() {
         Random colorRandom = new Random();
         int nextColor = colorRandom.nextInt(256);
-        switch (nextColor%16) {
-            case 10: return "a";
-            case 11: return "b";
-            case 12: return "c";
-            case 13: return "d";
-            case 14: return "e";
-            case 15: return "f";
-            default: return String.valueOf(nextColor%16);
+        switch (nextColor % 16) {
+            case 10:
+                return "a";
+            case 11:
+                return "b";
+            case 12:
+                return "c";
+            case 13:
+                return "d";
+            case 14:
+                return "e";
+            case 15:
+                return "f";
+            default:
+                return String.valueOf(nextColor % 16);
         }
     }
 
 
     /**
      * Creating map based on students answers
-     *
+     * <p>
      * Map looks like: Student name - List of student answers
+     *
      * @return the map of students answers
      */
     public HashMap<String, ArrayList<String>> getAnswersMap() {
@@ -355,17 +370,18 @@ public class TextModel {
         ArrayList<String> files = readAnswerFiles();
 
         //For each file get userName and name of task
-        for (String file: files) {
-            String[] objects = decodeStudentAnswerFile(file);
+        if (files != null)
+            for (String file : files) {
+                String[] objects = decodeStudentAnswerFile(file);
 
-            //If map contains key (user login) then add task to the list, else create this pair
-            if (answersMap.containsKey(objects[1]))
-                answersMap.get(objects[1]).add(objects[0]);
-            else {
-                answersMap.put(objects[1], new ArrayList<>());
-                answersMap.get(objects[1]).add(objects[0]);
+                //If map contains key (user login) then add task to the list, else create this pair
+                if (answersMap.containsKey(objects[1]))
+                    answersMap.get(objects[1]).add(objects[0]);
+                else {
+                    answersMap.put(objects[1], new ArrayList<>());
+                    answersMap.get(objects[1]).add(objects[0]);
+                }
             }
-        }
 
 
         return answersMap;
@@ -373,6 +389,7 @@ public class TextModel {
 
     /**
      * Reading file of student answer
+     *
      * @param userName the name of user
      * @param taskName the name of task which text need return
      * @return the text of answer
@@ -390,7 +407,7 @@ public class TextModel {
 
             scanner.useDelimiter("\n");
 
-            while(scanner.hasNext()) {
+            while (scanner.hasNext()) {
                 builder.append(scanner.next());
                 builder.append("\n");
             }
