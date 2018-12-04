52f96ae5eef33f4d909b82138e0530cc596f7a30
Alex <alex.A4.25@yandex.ru>
2018-12-04 22:54:07 +0300

    Created class of config

---
+++ src/main/java/com/alexa4/linguistic_project/data_stores/TaskConfig.java
@@ -0,0 +1,31 @@
+package com.alexa4.linguistic_project.data_stores;
+
+import java.util.ArrayList;
+
+
+/**
+ * Configuration class which contains information about flags
+ * which uses in the task
+ */
+public class TaskConfig {
+    public static class Flag {
+        public static final String SELF_TESTING = "SELF-TESTING";
+    }
+
+    private ArrayList<String> mFlags;
+
+    public TaskConfig() {
+        mFlags = new ArrayList<>();
+    }
+
+    /**
+     * Adding flag to configuration
+     * @param flag
+     */
+    public void addFlagToConfig(String flag) {
+        if (!mFlags.contains(flag))
+            mFlags.add(flag);
+    }
+
+
+}
